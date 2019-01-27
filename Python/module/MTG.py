6dd1cf0d26a2ec1e82f74aa8159dcb6f079c3e3d
IMGP3DEV <42507604+Imgp3Dev@users.noreply.github.com>
2019-01-27 12:19:01 -0600

    Added more comments to MTG.py

--- Python/module/MTG.py
+++ Python/module/MTG.py
@@ -5,17 +5,17 @@ import httplib
 import time
 def resend(url):
 s = socket.socket()
-data = HTTPConnection.set_debuglevel(3)
-socket.create_connection(url,[10[url, 80]])
+data = HTTPConnection.set_debuglevel(3) ##Uses debugging just for an advantage of seeing more traffic.
+socket.create_connection(url,[10[url, 80]]) ###Connects to IP & Port.###
 r.send(data)
 sleep(2)
 res = input("Set a timer for how long you want to log the traffic or press E if you want to go in live logging mode: ")
 if res == "e":
     s.listen()
-    data =  file object = open(Log.txt [,w][, 0 ])
+    data =  file object = open(Log.txt [,w][, 0 ]) ###Listens to traffic and resort to Creating Log.txt then output "Data" to it.
 else:
     s.listen()
     sleep(res)
-    data =  file object = open(Log.txt [,w][, 0 ])
+    data =  file object = open(Log.txt [,w][, 0 ]) ###Same goes for this.###
 
 
