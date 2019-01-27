9690d637e097aaebb8b93464f0cb8a515fe3fe7e
imgp3dev <arithmetic@protonmail.com>
2019-01-26 22:57:39 -0600

    Updated Python version.

--- Python/DrMITM.py
+++ Python/DrMITM.py
@@ -3,10 +3,14 @@ import time
 import socket
 import MTG
 url = input("Enter URL: ")
+print("sending request")
 payload = {"Testing"}
 r = requests.post(url, data=payload)
 print(r.text)
 time.sleep(1)
-print socket.gethostname(url)
+print("IP: "  socket.gethostname(url))
+print("Note: the traffic request is stored in request.txt")
+ip = print("Switching IP")
+ip = url
+print("Sending message to the Server")
 resend()
-
--- Python/module/MTG.py
+++ Python/module/MTG.py
@@ -2,10 +2,20 @@
 # > Getting Data that was sent to the server
 import socket
 import httplib
+import time
 def resend(url):
 s = socket.socket()
 data = HTTPConnection.set_debuglevel(3)
 socket.create_connection(url,[10[url, 80]])
 r.send(data)
+sleep(2)
+res = input("Set a timer for how long you want to log the traffic or press E if you want to go in live logging mode: ")
+if res == "e":
+    s.listen()
+    data =  file object = open(Log.txt [,w][, 0 ])
+else:
+    s.listen()
+    sleep(res)
+    data =  file object = open(Log.txt [,w][, 0 ])
 
 
