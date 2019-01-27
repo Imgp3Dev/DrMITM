b72c982e686ba85df982113cd055546498b3bb46
IMGP3DEV <42507604+Imgp3Dev@users.noreply.github.com>
2019-01-27 12:05:22 -0600

    Added comments to DrMITM.nim

--- src/DrMITM.nim
+++ src/DrMITM.nim
@@ -2,23 +2,23 @@ import httpclient
 import eop
 import json
 import os
-intro()
-var client = newHttpClient()
+intro()##Asks to enter URL##
+var client = newHttpClient() ##creates client##
 client.headers = newHttpHeaders({ "Content-Type": "application/json" })
 let body = %*{
-"data": "Test"
+"data": "Test" ##sets up body message##
 }
 let response = client.request(input, httpMethod = HttpPost, body = $body)
-request()
-echo response.status
+request() ##sends request
+echo response.status ##outputs status##
 if response.status == 200:
 proc createFile(dir: string){
-request.txt
+request.txt #if the status is 200 then it will create a request.txt file#
 }
-  output() 
-  resent()
-  switch()
-  Log() 
+  output() #Outputs IP.
+  resent() #Resends Data
+  switch() #Changes IP to original
+  Log() #Logs traffic
 else:
   echo "Sorry there's an error."
 
