6e19742bf09f0c2d46153dd542b6d243b6dde44d
IMGP3DEV <42507604+Imgp3Dev@users.noreply.github.com>
2019-01-27 12:07:21 -0600

    Update DrMITM.nim

--- src/DrMITM.nim
+++ src/DrMITM.nim
@@ -11,10 +11,11 @@ let body = %*{
 let response = client.request(input, httpMethod = HttpPost, body = $body)
 request() ##sends request
 echo response.status ##outputs status##
-if response.status == 200:
+if response.status == 200: 
 proc createFile(dir: string){
-request.txt #if the status is 200 then it will create a request.txt file#
+request.txt 
 }
+##If the status is 200 it creates a file called request.txt and start the other functions##
   output() #Outputs IP.
   resent() #Resends Data
   switch() #Changes IP to original
