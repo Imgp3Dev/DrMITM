8bee9d468ed5befbc5d49495d08e631e72ecfffe
imgp3dev <arithmetic@protonmail.com>
2019-01-30 17:08:58 -0600

    Adding Traffic Blocking.

--- Python/module/MTG.py
+++ Python/module/MTG.py
@@ -9,10 +9,18 @@ data = HTTPConnection.set_debuglevel(3) ##Uses debugging just for an advantage o
 socket.create_connection(url,[10[url, 80]]) ###Connects to IP & Port.###
 r.send(data)
 sleep(2)
-res = input("Set a timer for how long you want to log the traffic or press E if you want to go in live logging mode: ")
+res = input("Set a timer for how long you want to log the traffic or press E if you want to go in live logging mode or b to go into traffic blocking mode:  ")
 if res == "e":
     s.listen()
     data =  file object = open(Log.txt [,w][, 0 ]) ###Listens to traffic and resort to Creating Log.txt then output "Data" to it.
+if res == "b"
+num = print("Enter any number from 1-3: ")
+###^To make sure it doesnt execute for no reason###
+while (num < 10):
+    r.send(spam)
+  spam =  print("########################")
+    num = num + 1
+   resend() #### Sends Hashtags after stopping in traffic ####
 else:
     s.listen()
     sleep(res)
